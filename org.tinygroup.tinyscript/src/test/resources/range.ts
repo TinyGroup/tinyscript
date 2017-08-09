b555fda29f08bf007e18fff2dfdf9953d481470e
aotianji1238 <aotianji1238@163.com>
2017-08-09 22:10:06 +0800

    [测试] 增加range的测试用例

---
+++ org.tinygroup.tinyscript/src/test/resources/range.ts
@@ -0,0 +1,23 @@
+//输出int类型
+list = [ 8 .. 18];
+println(list);
+list = [ 18 .. 8];
+println(list);
+list = [ 8 .. 8];
+println(list);
+
+//输出long类型
+list = [ 100L .. 110L];
+println(list);
+list = [ 210L .. 200L];
+println(list);
+list = [ 1L .. 1L];
+println(list);
+
+//输出char类型
+list = [ 'a' .. 'z'];
+println(list);
+list = [ 'z' .. 'a'];
+println(list);
+list = [ 'm' .. 'm'];
+println(list);
