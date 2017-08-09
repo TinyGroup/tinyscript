0d7bc202bd673db07c453967786be62fecdcb054
hspcadmin <hspcadmin@WINDOWS-VIFI184>
2017-08-09 09:41:08 +0800

    修改update，可以支持lamdba

--- org.tinygroup.tinyscript.dataset/src/main/java/org/tinygroup/tinyscript/dataset/function/DataSetUpdateFunction.java
+++ org.tinygroup.tinyscript.dataset/src/main/java/org/tinygroup/tinyscript/dataset/function/DataSetUpdateFunction.java
@@ -13,101 +13,124 @@ import org.tinygroup.tinyscript.dataset.GroupDataSet;
 import org.tinygroup.tinyscript.dataset.util.DataSetUtil;
 import org.tinygroup.tinyscript.function.AbstractScriptFunction;
 import org.tinygroup.tinyscript.impl.DefaultScriptContext;
+import org.tinygroup.tinyscript.interpret.LambdaFunction;
+import org.tinygroup.tinyscript.interpret.ScriptResult;
+import org.tinygroup.tinyscript.interpret.call.FunctionCallExpressionParameter;
 
 public class DataSetUpdateFunction extends AbstractScriptFunction {
 
 	public String getNames() {
 		return "update";
 	}
-	
+
 	public String getBindingTypes() {
 		return DataSet.class.getName();
 	}
-	
-	public boolean  enableExpressionParameter(){
+
+	public boolean enableExpressionParameter() {
 		return true;
 	}
 
-	public Object execute(ScriptSegment segment, ScriptContext context,
-			Object... parameters) throws ScriptException {
-		try{
+	public Object execute(ScriptSegment segment, ScriptContext context, Object... parameters) throws ScriptException {
+		try {
 			if (parameters == null || parameters.length == 0) {
 				throw new ScriptException("update函数的参数为空!");
-			}else if( checkParameters(parameters, 3) ){
+			} else if (checkParameters(parameters, 3)) {
 				AbstractDataSet dataSet = (AbstractDataSet) getValue(parameters[0]);
-				String  colName = (String) getValue(parameters[1]);
-				return updateExpression(dataSet,colName,getExpression(parameters[2]),context);
-			}else{
+				String colName = (String) getValue(parameters[1]);
+
+				return updateExpression(dataSet, colName, parameters[2], context);
+			} else {
 				throw new ScriptException("update函数的参数格式不正确!");
 			}
-		}catch (ScriptException e) {
+		} catch (ScriptException e) {
 			throw e;
 		} catch (Exception e) {
 			throw new ScriptException("update函数的参数格式不正确!", e);
 		}
 	}
-	
-	private DataSet updateExpression(AbstractDataSet dataSet,String colName,String expression,ScriptContext context) throws ScriptException {
+
+	private DataSet updateExpression(AbstractDataSet dataSet, String colName, Object parameter, ScriptContext context)
+			throws ScriptException {
+		String expression = null;
+
 		try {
+			expression = getExpression(parameter);
 			int col = DataSetUtil.getFieldIndex(dataSet, colName);
-			if(col<0){
-			   throw new ScriptException(String.format("根据字段%s没有在数据集找到匹配列", colName));
+			if (col < 0) {
+				throw new ScriptException(String.format("根据字段%s没有在数据集找到匹配列", colName));
 			}
-			
-			//获取需要数组处理的字段
-			Set<Integer> columns = DataSetUtil.getFieldArray(dataSet, expression);
-			
-			//转换表达式为脚本可以执行的语法片段
+
+			Set<Integer> columns = null;
+
+			// 获取需要数组处理的字段
+			columns = DataSetUtil.getFieldArray(dataSet, expression);
+
+			// 转换表达式为脚本可以执行的语法片段
 			expression = DataSetUtil.convertExpression(expression);
-			
-			
-			if(dataSet instanceof GroupDataSet){
+
+			Object function = null;
+			if (expression.startsWith("("))
+				function = ((FunctionCallExpressionParameter) (parameter)).eval();
+			else
+				function = expression;
+
+			if (dataSet instanceof GroupDataSet) {
 				GroupDataSet groupDataSet = (GroupDataSet) dataSet;
-				for(DynamicDataSet subDs:groupDataSet.getGroups()){
-					updateRowWithExpression(subDs,col,expression,columns,context);
+				for (DynamicDataSet subDs : groupDataSet.getGroups()) {
+					updateRowWithExpression(subDs, col, function, columns, context);
 				}
-			}else{
-				updateRowWithExpression(dataSet,col,expression,columns,context);
+			} else {
+				updateRowWithExpression(dataSet, col, function, columns, context);
 			}
-			
-		    return dataSet;
-			
+
+			return dataSet;
+
 		} catch (ScriptException e) {
 			throw e;
 		} catch (Exception e) {
-			throw new ScriptException("执行update函数发生异常",e);
+			throw new ScriptException("执行update函数发生异常", e);
 		}
 	}
-	
-	//参数为实际下标
-	private void updateRowWithExpression(AbstractDataSet dataSet,int col,String expression,Set<Integer> columns,ScriptContext context) throws Exception{
-		for(int i=0;i<dataSet.getRows();i++){
+
+	// 参数为实际下标
+	private void updateRowWithExpression(AbstractDataSet dataSet, int col, Object expression, Set<Integer> columns,
+			ScriptContext context) throws Exception {
+		for (int i = 0; i < dataSet.getRows(); i++) {
 			ScriptContext subContext = new DefaultScriptContext();
 			subContext.setParent(context);
 			subContext.put("$currentRow", dataSet.getShowIndex(i));
-			setRowValue(subContext,dataSet,columns,i);
+			setRowValue(subContext, dataSet, columns, i);
 			Object value;
-			try{
-				value =  executeDynamicObject(expression,subContext);
-				dataSet.setData(dataSet.getShowIndex(i), dataSet.getShowIndex(col),value);
-			}catch(ScriptException e){
-				//忽略脚本异常
+			try {
+				if (expression instanceof LambdaFunction) {
+					value = ((LambdaFunction) expression).execute(subContext, i + 1);
+					dataSet.setData(dataSet.getShowIndex(i), dataSet.getShowIndex(col),
+							((ScriptResult) value).getResult());
+				} else {
+					value = executeDynamicObject((String) expression, subContext);
+					dataSet.setData(dataSet.getShowIndex(i), dataSet.getShowIndex(col), value);
+				}
+
+			} catch (ScriptException e) {
+				// 忽略脚本异常
 			}
-			
+
 		}
 	}
-	
-	private void setRowValue(ScriptContext context,AbstractDataSet dataSet,Set<Integer> columns,int row) throws Exception{
-		for(int j=0;j<dataSet.getColumns();j++){
+
+	private void setRowValue(ScriptContext context, AbstractDataSet dataSet, Set<Integer> columns, int row)
+			throws Exception {
+		for (int j = 0; j < dataSet.getColumns(); j++) {
 			Field field = dataSet.getFields().get(j);
-			if(columns!=null && columns.contains(j)){
-			   //存在字段数组形式
-				context.put(field.getName(),DataSetUtil.createDataSetColumn(dataSet, dataSet.getShowIndex(j)));
-			}else{
-			   //直接赋值
-				context.put(field.getName(), dataSet.getData(dataSet.getShowIndex(row), dataSet.getShowIndex(j)));  //设置行参数上下文
+			if (columns != null && columns.contains(j)) {
+				// 存在字段数组形式
+				context.put(field.getName(), DataSetUtil.createDataSetColumn(dataSet, dataSet.getShowIndex(j)));
+			} else {
+				// 直接赋值
+				context.put(field.getName(), dataSet.getData(dataSet.getShowIndex(row), dataSet.getShowIndex(j))); // 设置行参数上下文
 			}
-			
+
 		}
 	}
 
--- org.tinygroup.tinyscript.dataset/src/main/java/org/tinygroup/tinyscript/dataset/util/DataSetUtil.java
+++ org.tinygroup.tinyscript.dataset/src/main/java/org/tinygroup/tinyscript/dataset/util/DataSetUtil.java
@@ -25,6 +25,7 @@ import org.tinygroup.tinyscript.expression.ExpressionUtil;
 
 /**
  * 结果集工具类
+ * 
  * @author yancheng11334
  *
  */
@@ -32,348 +33,374 @@ public final class DataSetUtil {
 
 	private static final Pattern EXCEL_ITEM = Pattern.compile("[a-zA-Z]{1,2}[0-9]{1,5}");
 	private static final Pattern EXCEL_CHAR = Pattern.compile("[a-zA-Z]{1,2}");
+
 	/**
 	 * 构建动态数据集
+	 * 
 	 * @param fields
 	 * @param dataArray
 	 * @return
 	 * @throws Exception
 	 */
-	public static DynamicDataSet createDynamicDataSet(List<Field> fields,Object[][] dataArray) throws Exception{
-		return new SimpleDataSet(fields,dataArray);
+	public static DynamicDataSet createDynamicDataSet(List<Field> fields, Object[][] dataArray) throws Exception {
+		return new SimpleDataSet(fields, dataArray);
 	}
-	
+
 	/**
 	 * 构建动态数据集
+	 * 
 	 * @param fields
 	 * @param dataArray
 	 * @param tag
 	 * @return
 	 * @throws Exception
 	 */
-	public static DynamicDataSet createDynamicDataSet(List<Field> fields,Object[][] dataArray,boolean tag) throws Exception{
-		return new SimpleDataSet(fields,dataArray,tag);
+	public static DynamicDataSet createDynamicDataSet(List<Field> fields, Object[][] dataArray, boolean tag)
+			throws Exception {
+		return new SimpleDataSet(fields, dataArray, tag);
 	}
-	
+
 	/**
 	 * 构建动态数据集
+	 * 
 	 * @param fields
 	 * @param dataSetRows
 	 * @return
 	 * @throws Exception
 	 */
-	public static DynamicDataSet createDynamicDataSet(List<Field> fields,List<DataSetRow> dataSetRows) throws Exception{
+	public static DynamicDataSet createDynamicDataSet(List<Field> fields, List<DataSetRow> dataSetRows)
+			throws Exception {
 		Object[][] dataArray = new Object[dataSetRows.size()][];
-		for(int i=0;i<dataSetRows.size();i++){
+		for (int i = 0; i < dataSetRows.size(); i++) {
 			DataSetRow dataSetRow = dataSetRows.get(i);
 			dataArray[i] = new Object[fields.size()];
-			for(int j=0;j<fields.size();j++){
+			for (int j = 0; j < fields.size(); j++) {
 				Field field = fields.get(j);
 				dataArray[i][j] = (Object) dataSetRow.getData(field.getName());
 			}
 		}
-		return new SimpleDataSet(fields,dataArray);
+		return new SimpleDataSet(fields, dataArray);
 	}
-	
+
 	/**
 	 * 构建动态数据集
+	 * 
 	 * @param dataSet
 	 * @param row
 	 * @return
 	 * @throws Exception
 	 */
-	public static DynamicDataSet createDynamicDataSet(DataSet dataSet,int row) throws Exception{
+	public static DynamicDataSet createDynamicDataSet(DataSet dataSet, int row) throws Exception {
 		Object[][] dataArray = new Object[1][];
 		dataArray[0] = new Object[dataSet.getColumns()];
 		AbstractDataSet abstractDataSet = (AbstractDataSet) dataSet;
-		for(int i=0;i<dataArray[0].length;i++){
+		for (int i = 0; i < dataArray[0].length; i++) {
 			dataArray[0][i] = dataSet.getData(abstractDataSet.getShowIndex(row), abstractDataSet.getShowIndex(i));
 		}
 		List<Field> fields = new ArrayList<Field>();
-		for(Field field:dataSet.getFields()){
+		for (Field field : dataSet.getFields()) {
 			fields.add(field);
 		}
-		DynamicDataSet result = new SimpleDataSet(fields,dataArray);
+		DynamicDataSet result = new SimpleDataSet(fields, dataArray);
 		result.setIndexFromOne(dataSet.isIndexFromOne());
 		return result;
 	}
-	
+
 	/**
 	 * 构建动态数据集(保持父数据集副本,关联修改)
+	 * 
 	 * @param dataSet
 	 * @param rows
 	 * @return
 	 * @throws Exception
 	 */
-	public static DynamicDataSet createDynamicDataSet(DataSet dataSet,List<Integer> rows) throws Exception{
+	public static DynamicDataSet createDynamicDataSet(DataSet dataSet, List<Integer> rows) throws Exception {
 		SimpleDataSet localDataSet = getSimpleDataSet(dataSet);
-		
-		if(localDataSet==null){
-		   throw new ScriptException(String.format("%s类型的数据集无法保持副本,建议转换成SimpleDataSet类型", dataSet.getClass().getName()));
+
+		if (localDataSet == null) {
+			throw new ScriptException(
+					String.format("%s类型的数据集无法保持副本,建议转换成SimpleDataSet类型", dataSet.getClass().getName()));
 		}
-		
+
 		Object[][] fatherArray = localDataSet.getDataArray();
-		
-		if(rows==null){
-			return new SimpleDataSet(dataSet.getFields(),fatherArray,dataSet.isIndexFromOne());
-		}else{
+
+		if (rows == null) {
+			return new SimpleDataSet(dataSet.getFields(), fatherArray, dataSet.isIndexFromOne());
+		} else {
 			Object[][] childArray = new Object[rows.size()][];
-			for(int i=0;i<rows.size();i++){
+			for (int i = 0; i < rows.size(); i++) {
 				childArray[i] = fatherArray[rows.get(i)];
 			}
-			return new SimpleDataSet(dataSet.getFields(),childArray,dataSet.isIndexFromOne());
+			return new SimpleDataSet(dataSet.getFields(), childArray, dataSet.isIndexFromOne());
 		}
 	}
-	
+
 	/**
 	 * 构建动态数据集(保持父数据集副本,关联修改)
+	 * 
 	 * @param dataSet
 	 * @param beginIndex
 	 * @param endIndex
 	 * @return
 	 * @throws Exception
 	 */
-	public static DynamicDataSet createDynamicDataSet(DataSet dataSet,int beginIndex,int endIndex) throws Exception{
+	public static DynamicDataSet createDynamicDataSet(DataSet dataSet, int beginIndex, int endIndex) throws Exception {
 		beginIndex = beginIndex > 0 ? beginIndex : 0;
-		endIndex   = endIndex< dataSet.getRows() ? endIndex : dataSet.getRows() -1 ;
-		
-        SimpleDataSet localDataSet = getSimpleDataSet(dataSet);
-		
-		if(localDataSet==null){
-		   throw new ScriptException(String.format("%s类型的数据集无法保持副本,建议转换成SimpleDataSet类型", dataSet.getClass().getName()));
+		endIndex = endIndex < dataSet.getRows() ? endIndex : dataSet.getRows() - 1;
+
+		SimpleDataSet localDataSet = getSimpleDataSet(dataSet);
+
+		if (localDataSet == null) {
+			throw new ScriptException(
+					String.format("%s类型的数据集无法保持副本,建议转换成SimpleDataSet类型", dataSet.getClass().getName()));
 		}
-		
+
 		Object[][] fatherArray = localDataSet.getDataArray();
-		Object[][] childArray = new Object[endIndex-beginIndex+1][];
-		for(int i=beginIndex;i<=endIndex;i++){
-		   childArray[i-beginIndex] = fatherArray[i];
+		Object[][] childArray = new Object[endIndex - beginIndex + 1][];
+		for (int i = beginIndex; i <= endIndex; i++) {
+			childArray[i - beginIndex] = fatherArray[i];
 		}
-		return new SimpleDataSet(dataSet.getFields(),childArray,dataSet.isIndexFromOne());
+		return new SimpleDataSet(dataSet.getFields(), childArray, dataSet.isIndexFromOne());
 	}
-	
+
 	private static SimpleDataSet getSimpleDataSet(DataSet dataSet) throws Exception {
 		SimpleDataSet localDataSet = null;
-		if(dataSet instanceof SimpleDataSet){
+		if (dataSet instanceof SimpleDataSet) {
 			localDataSet = (SimpleDataSet) dataSet;
-		}else if(dataSet instanceof VariableDataSet){
+		} else if (dataSet instanceof VariableDataSet) {
 			VariableDataSet variableDataSet = (VariableDataSet) dataSet;
-			if(variableDataSet.getVariableDataSet() instanceof SimpleDataSet){
-			   localDataSet = (SimpleDataSet) variableDataSet.getVariableDataSet();
+			if (variableDataSet.getVariableDataSet() instanceof SimpleDataSet) {
+				localDataSet = (SimpleDataSet) variableDataSet.getVariableDataSet();
 			}
 		}
 		return localDataSet;
 	}
-	
+
 	/**
 	 * 构建数据集的行对象
+	 * 
 	 * @param dataSet
 	 * @param row
 	 * @return
 	 * @throws Exception
 	 */
-	public static DataSetRow createDataSetRow(DataSet dataSet,int row) throws Exception{
-		return new DefaultDataSetRow(dataSet,row);
+	public static DataSetRow createDataSetRow(DataSet dataSet, int row) throws Exception {
+		return new DefaultDataSetRow(dataSet, row);
 	}
-	
+
 	/**
 	 * 构建数据集的列对象
+	 * 
 	 * @param dataSet
 	 * @param col
 	 * @return
 	 * @throws Exception
 	 */
-	public static DataSetColumn createDataSetColumn(DataSet dataSet,int col) throws Exception{
-		return new DefaultDataSetColumn(dataSet,col);
+	public static DataSetColumn createDataSetColumn(DataSet dataSet, int col) throws Exception {
+		return new DefaultDataSetColumn(dataSet, col);
 	}
-	
+
 	/**
 	 * 构建数据集的列对象
+	 * 
 	 * @param dataSet
 	 * @param colName
 	 * @return
 	 * @throws Exception
 	 */
-	public static DataSetColumn createDataSetColumn(DataSet dataSet,String colName) throws Exception{
-		return new DefaultDataSetColumn(dataSet,colName);
+	public static DataSetColumn createDataSetColumn(DataSet dataSet, String colName) throws Exception {
+		return new DefaultDataSetColumn(dataSet, colName);
 	}
-	
+
 	/**
 	 * 返回索引字段对应的列
+	 * 
 	 * @param dataSet
 	 * @param colName
 	 * @return
 	 * @throws Exception
 	 */
-	public static int getFieldIndex(DataSet dataSet,String colName) throws Exception{
-		for(int i=0;i<dataSet.getFields().size();i++){
+	public static int getFieldIndex(DataSet dataSet, String colName) throws Exception {
+		for (int i = 0; i < dataSet.getFields().size(); i++) {
 			Field field = dataSet.getFields().get(i);
-			if(field.getName().equalsIgnoreCase(colName)){
-			   return i;
+			if (field.getName().equalsIgnoreCase(colName)) {
+				return i;
 			}
 		}
-		if(dataSet instanceof GroupDataSet){
-			GroupDataSet groupDataSet =(GroupDataSet) dataSet;
+		if (dataSet instanceof GroupDataSet) {
+			GroupDataSet groupDataSet = (GroupDataSet) dataSet;
 			List<AggregateResult> result = groupDataSet.getAggregateResultList();
-			for(int i=0;i<result.size();i++){
-				if(colName.equalsIgnoreCase(result.get(i).getName())){
-				   return groupDataSet.getFields().size()+i;
+			for (int i = 0; i < result.size(); i++) {
+				if (colName.equalsIgnoreCase(result.get(i).getName())) {
+					return groupDataSet.getFields().size() + i;
 				}
 			}
 		}
 		return -1;
 	}
-	
+
 	/**
 	 * 设置行上下文参数
+	 * 
 	 * @param dataSet
 	 * @param context
 	 * @param row
 	 * @throws Exception
 	 */
-	public static void setRowContext(AbstractDataSet dataSet,ScriptContext context,int row) throws Exception{
-		//将行记录按名称放入到上下文
-		for(int j=0;j<dataSet.getFields().size();j++){
+	public static void setRowContext(AbstractDataSet dataSet, ScriptContext context, int row) throws Exception {
+		// 将行记录按名称放入到上下文
+		for (int j = 0; j < dataSet.getFields().size(); j++) {
 			String name = dataSet.getFields().get(j).getName();
-			context.put(name, dataSet.getData(dataSet.getShowIndex(row), dataSet.getShowIndex(j)));	
+			context.put(name, dataSet.getData(dataSet.getShowIndex(row), dataSet.getShowIndex(j)));
 		}
-		if(dataSet instanceof GroupDataSet){
+		if (dataSet instanceof GroupDataSet) {
 			GroupDataSet groupDataSet = (GroupDataSet) dataSet;
-			for(AggregateResult result:groupDataSet.getAggregateResultList()){
+			for (AggregateResult result : groupDataSet.getAggregateResultList()) {
 				context.put(result.getName(), result.getData(row));
 			}
 		}
 	}
-	
+
 	/**
 	 * 清理行上下文参数(仅清理当前层次的上下文)
+	 * 
 	 * @param dataSet
 	 * @param context
 	 * @throws Exception
 	 */
-	public static void clearRowContext(AbstractDataSet dataSet,ScriptContext context) throws Exception{
-		for(int j=0;j<dataSet.getFields().size();j++){
+	public static void clearRowContext(AbstractDataSet dataSet, ScriptContext context) throws Exception {
+		for (int j = 0; j < dataSet.getFields().size(); j++) {
 			String name = dataSet.getFields().get(j).getName();
 			context.getItemMap().remove(name);
 		}
-		if(dataSet instanceof GroupDataSet){
+		if (dataSet instanceof GroupDataSet) {
 			GroupDataSet groupDataSet = (GroupDataSet) dataSet;
-			for(AggregateResult result:groupDataSet.getAggregateResultList()){
+			for (AggregateResult result : groupDataSet.getAggregateResultList()) {
 				context.getItemMap().remove(result.getName());
 			}
 		}
 	}
-	
+
 	/**
 	 * 判断是否整型
+	 * 
 	 * @param item
 	 * @return
 	 */
-	public static boolean isNumber(Object item){
-		try{
-			return ExpressionUtil.convertInteger(item)!=null;
-		}catch(Exception e){
+	public static boolean isNumber(Object item) {
+		try {
+			return ExpressionUtil.convertInteger(item) != null;
+		} catch (Exception e) {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * 获取值
+	 * 
 	 * @param item
 	 * @param context
 	 * @return
 	 * @throws Exception
 	 */
-	public static Object getValue(Object item,ScriptContext context) throws Exception {
-		if(isNumber(item)){
-		   return ExpressionUtil.convertInteger(item);
+	public static Object getValue(Object item, ScriptContext context) throws Exception {
+		if (isNumber(item)) {
+			return ExpressionUtil.convertInteger(item);
 		}
 		Object value = context.get(item.toString());
-		if(value!=null){
-		   return value;
+		if (value != null) {
+			return value;
 		}
 		return item;
 	}
 
 	/**
 	 * 判断是否字段(注意包含Excel访问方式)
+	 * 
 	 * @param item
 	 * @param context
 	 * @return
 	 */
-	public static boolean isField(Object item,ScriptContext context){
-		if(isNumber(item)){
-		   return false;
+	public static boolean isField(Object item, ScriptContext context) {
+		if (isNumber(item)) {
+			return false;
 		}
 		Object value = context.get(item.toString());
-		if(value!=null){
+		if (value != null) {
 			return !isNumber(value);
 		}
 		return true;
 	}
-	
+
 	/**
 	 * 判断是否Excel单元格下标方式
+	 * 
 	 * @param s
 	 * @return
 	 */
-	public static boolean isExcelCell(String s){
+	public static boolean isExcelCell(String s) {
 		return EXCEL_ITEM.matcher(s).matches();
 	}
-	
+
 	/**
 	 * 获取Excel单元格下标方式
+	 * 
 	 * @param s
 	 * @return
 	 */
-	public static int[] getExcelCell(String s){
+	public static int[] getExcelCell(String s) {
 		int[] cell = new int[2];
-		try{
+		try {
 			s = s.toUpperCase();
 			Matcher m = EXCEL_CHAR.matcher(s);
 			m.find();
 			char[] cs = m.group().toCharArray();
-			if(cs.length==1){
-				cell[0] = (int)cs[0] - 65;
-			}else{
-				cell[0] = ((int)cs[0] - 64)*26+((int)cs[1] - 65);
+			if (cs.length == 1) {
+				cell[0] = (int) cs[0] - 65;
+			} else {
+				cell[0] = ((int) cs[0] - 64) * 26 + ((int) cs[1] - 65);
 			}
-			cell[1] = Integer.parseInt(s.substring(cs.length,s.length())) -1;
-		}catch(Exception e){
-		   throw new RuntimeException(String.format("转换Excel单元格下标方式失败:坐标[%s]", s));
+			cell[1] = Integer.parseInt(s.substring(cs.length, s.length())) - 1;
+		} catch (Exception e) {
+			throw new RuntimeException(String.format("转换Excel单元格下标方式失败:坐标[%s]", s));
 		}
-		
+
 		return cell;
 	}
-	
+
 	/**
 	 * 转换一般表达式为可执行脚本
+	 * 
 	 * @param expression
 	 * @return
 	 */
-	public static String convertExpression(String expression){
-		if(!expression.startsWith("return")){
-			expression = "return "+expression;
+	public static String convertExpression(String expression) {
+		if (expression.startsWith("("))
+			return expression;
+		if (!expression.startsWith("return")) {
+			expression = "return " + expression;
 		}
-		if(!expression.endsWith(";")){
-			expression = expression+";";
+		if (!expression.endsWith(";")) {
+			expression = expression + ";";
 		}
 		return expression;
 	}
-	
+
 	/**
 	 * 取得需要进行数组处理的字段
+	 * 
 	 * @param dataSet
 	 * @param expression
 	 * @return
 	 * @throws Exception
 	 */
-	public static Set<Integer> getFieldArray(DataSet dataSet,String expression) throws Exception{
+	public static Set<Integer> getFieldArray(DataSet dataSet, String expression) throws Exception {
 		Set<Integer> columns = new HashSet<Integer>();
-		for(int i=0;i<dataSet.getFields().size();i++){
+		for (int i = 0; i < dataSet.getFields().size(); i++) {
 			Field field = dataSet.getFields().get(i);
-			Pattern FIELD_ARRAY_RULE = Pattern.compile(field.getName()+"\\[(-)?[_a-zA-Z0-9\u4E00-\u9FA5]*\\]");
-			if(FIELD_ARRAY_RULE.matcher(expression).find()){
-			   columns.add(i);
+			Pattern FIELD_ARRAY_RULE = Pattern.compile(field.getName() + "\\[(-)?[_a-zA-Z0-9\u4E00-\u9FA5]*\\]");
+			if (FIELD_ARRAY_RULE.matcher(expression).find()) {
+				columns.add(i);
 			}
 		}
 		return columns;
