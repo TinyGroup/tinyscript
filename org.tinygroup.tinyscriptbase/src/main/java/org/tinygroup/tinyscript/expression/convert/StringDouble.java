5a3172a06850ef16ef50156e8c698670578666bd
fujie <405295521@qq.com>
2017-08-10 19:55:34 +0800

    修改double和float，可以转换百分数

--- org.tinygroup.tinyscriptbase/src/main/java/org/tinygroup/tinyscript/expression/convert/StringDouble.java
+++ org.tinygroup.tinyscriptbase/src/main/java/org/tinygroup/tinyscript/expression/convert/StringDouble.java
@@ -2,9 +2,13 @@ package org.tinygroup.tinyscript.expression.convert;
 
 import org.tinygroup.tinyscript.expression.Converter;
 
-public class StringDouble implements Converter<String,Double> {
+public class StringDouble implements Converter<String, Double> {
 
 	public Double convert(String object) {
+		if (object.indexOf("%") > 0) {
+			object = object.replaceAll("%", "");
+			return Double.parseDouble(object)/100;
+		}
 		return Double.parseDouble(object);
 	}
 
