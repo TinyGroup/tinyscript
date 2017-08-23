170d3613abb6259be23c22d355e82ff129e68cd8
fujie <405295521@qq.com>
2017-08-23 20:55:21 +0800

    优化dataSet写法

--- org.tinygroup.tinyscript/src/test/resources/example/dataSetDpKnapsack.ts
+++ org.tinygroup.tinyscript/src/test/resources/example/dataSetDpKnapsack.ts
@@ -10,14 +10,14 @@ println(dataSet.dpKnapsack(10,dataSet.weight,dataSet.value));
 
 
 dataSet = readTxt("/example/knapsack2.txt");
-dataSet = dataSet.double("value").int("weight").int("count");
+dataSet = dataSet.double("value").int("weight","count");
 //================================================================================
 //多重背包
 println(dataSet.dpKnapsack(15,dataSet.weight,dataSet.count,dataSet.value));
 
 
 dataSet = readTxt("/example/knapsack3.txt");
-dataSet = dataSet.double("value").int("weight").int("count");
+dataSet = dataSet.double("value").int("weight","count");
 //================================================================================
 //混合背包
 println(dataSet.dpKnapsack(15,dataSet.weight,dataSet.count,dataSet.value));
@@ -25,7 +25,7 @@ println(dataSet.dpKnapsack(15,dataSet.weight,dataSet.count,dataSet.value));
 
 //基金问题
 dataSet = readTxt("/example/knapsack4.txt");
-dataSet = dataSet.int("minmoney").int("maxmoney").double("interestRate");
+dataSet = dataSet.int("minmoney","maxmoney").double("interestRate");
 dataSet.insertColumn(1,"value").insertColumn(2,"count").double("value").int("count");
 dataSet.update("value",interestRate[0]*minmoney[0]*80);
 dataSet.update("count",maxmoney[0]/minmoney[0]);
@@ -35,7 +35,7 @@ println(dataSet.dpKnapsack(5000,dataSet.minmoney,dataSet.count,dataSet.value));
 
 //购物单问题
 dataSet = readTxt("/example/knapsack5.txt");
-dataSet = dataSet.int("price").int("importance").int("rule");
+dataSet = dataSet.int("price","importance","rule");
 dataSet.insertColumn(1,"value").double("value");
 dataSet.update("value",price[0]*importance[0]);
 //================================================================================
