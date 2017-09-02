e849905abda862f9fa2860e3a83bab4db39d2f7f
fujie <405295521@qq.com>
2017-09-02 20:40:06 +0800

    1.filterGroup现支持lambda表达式。2.修改相关测试用例，增加复杂sql测试用例

--- org.tinygroup.tinyscript.dataset/src/main/java/org/tinygroup/tinyscript/dataset/function/GroupDataSetFilterFunction.java
+++ org.tinygroup.tinyscript.dataset/src/main/java/org/tinygroup/tinyscript/dataset/function/GroupDataSetFilterFunction.java
@@ -1,6 +1,7 @@
 package org.tinygroup.tinyscript.dataset.function;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -16,86 +17,91 @@ import org.tinygroup.tinyscript.dataset.impl.DefaultGroupDataSet;
 import org.tinygroup.tinyscript.dataset.util.DataSetUtil;
 import org.tinygroup.tinyscript.function.AbstractScriptFunction;
 import org.tinygroup.tinyscript.impl.DefaultScriptContext;
-import org.tinygroup.tinyscript.interpret.ScriptContextUtil;
+import org.tinygroup.tinyscript.interpret.LambdaFunction;
+import org.tinygroup.tinyscript.interpret.call.FunctionCallExpressionParameter;
 
 public class GroupDataSetFilterFunction extends AbstractScriptFunction {
 
 	public String getNames() {
 		return "filterGroup";
 	}
-	
+
 	public String getBindingTypes() {
 		return GroupDataSet.class.getName();
 	}
-	
-	public boolean  enableExpressionParameter(){
+
+	public boolean enableExpressionParameter() {
 		return true;
 	}
 
-	public Object execute(ScriptSegment segment, ScriptContext context,
-			Object... parameters) throws ScriptException {
-		try{
-        	if (parameters == null || parameters.length == 0) {
+	public Object execute(ScriptSegment segment, ScriptContext context, Object... parameters) throws ScriptException {
+		try {
+			if (parameters == null || parameters.length == 0) {
 				throw new ScriptException("filterGroup函数的参数为空!");
-			} else if(checkParameters(parameters, 2)){
-				 GroupDataSet groupDataSet = (GroupDataSet)getValue(parameters[0]);
-				 String expression = getExpression(parameters[1]);
-				 return filterGroup(context,groupDataSet,expression);
+			} else if (checkParameters(parameters, 2)) {
+				GroupDataSet groupDataSet = (GroupDataSet) getValue(parameters[0]);
+				LambdaFunction expression = (LambdaFunction) (((FunctionCallExpressionParameter) parameters[1]).eval());
+				return filterGroup(context, groupDataSet, expression);
 			} else {
 				throw new ScriptException("filterGroup函数的参数格式不正确");
 			}
-				
-        }catch (ScriptException e) {
+
+		} catch (ScriptException e) {
 			throw e;
 		} catch (Exception e) {
 			throw new ScriptException("filterGroup函数的参数格式不正确!", e);
 		}
 	}
-	
-	private GroupDataSet filterGroup(ScriptContext context,GroupDataSet groupDataSet,String expression) throws Exception{
-		
-		Set<Integer> columns = DataSetUtil.getFieldArray(groupDataSet, expression);
-		expression = ScriptContextUtil.convertExpression(expression);
-		
+
+	private Object filterGroup(ScriptContext context, GroupDataSet groupDataSet, LambdaFunction expression)
+			throws Exception {
 		List<DynamicDataSet> newList = new ArrayList<DynamicDataSet>();
-		
-		for(int dsNum =0; dsNum<groupDataSet.getGroups().size(); dsNum++){
+
+		Set<Integer> columns = new HashSet<Integer>();
+		for (int j = 0; j < groupDataSet.getFields().size(); j++) {
+			columns.add(j + 1);
+		}
+
+		for (int dsNum = 0; dsNum < groupDataSet.getGroups().size(); dsNum++) {
 			List<Integer> matchRows = new ArrayList<Integer>();
 			DynamicDataSet subDs = groupDataSet.getGroups().get(dsNum);
-			for(int i=0;i<subDs.getRows();i++){
-			   ScriptContext subContext = new DefaultScriptContext();
-			   subContext.setParent(context);
-			   subContext.put("$currentRow", groupDataSet.getShowIndex(i));
-			   setRowValue(subContext,subDs,columns,i);
-			   setAggregateValue(subContext,subDs,groupDataSet.getAggregateResultList(),dsNum);
-			   if(executeDynamicBoolean(expression, subContext)){
-				  matchRows.add(i);
-			   }
+			for (int i = 0; i < subDs.getRows(); i++) {
+				ScriptContext subContext = new DefaultScriptContext();
+				subContext.setParent(context);
+				subContext.put("$currentRow", groupDataSet.getShowIndex(i));
+				setRowValue(subContext, subDs, columns, i);
+				setAggregateValue(subContext, subDs, groupDataSet.getAggregateResultList(), dsNum);
+				if ((Boolean) expression.execute(subContext).getResult()) {
+					matchRows.add(i);
+				}
 			}
-			if(!matchRows.isEmpty()){
+			if (!matchRows.isEmpty()) {
 				newList.add(DataSetUtil.createDynamicDataSet(subDs, matchRows));
 			}
 		}
-		return new DefaultGroupDataSet(groupDataSet.getFields(),newList,groupDataSet.getAggregateResultList(),groupDataSet.isIndexFromOne());
+		return new DefaultGroupDataSet(groupDataSet.getFields(), newList, groupDataSet.getAggregateResultList(),
+				groupDataSet.isIndexFromOne());
 	}
-	
-	private void setAggregateValue(ScriptContext context,AbstractDataSet dataSet,List<AggregateResult> aggregateResultList,int row) throws Exception{
-		for(AggregateResult result:aggregateResultList){
+
+	private void setAggregateValue(ScriptContext context, AbstractDataSet dataSet,
+			List<AggregateResult> aggregateResultList, int row) throws Exception {
+		for (AggregateResult result : aggregateResultList) {
 			context.put(result.getName(), result.getData(row));
 		}
 	}
-	
-	private void setRowValue(ScriptContext context,AbstractDataSet dataSet,Set<Integer> columns,int row) throws Exception{
-		for(int j=0;j<dataSet.getColumns();j++){
+
+	private void setRowValue(ScriptContext context, AbstractDataSet dataSet, Set<Integer> columns, int row)
+			throws Exception {
+		for (int j = 0; j < dataSet.getColumns(); j++) {
 			Field field = dataSet.getFields().get(j);
-			if(columns!=null && columns.contains(j)){
-			   //存在字段数组形式
-				context.put(field.getName(),DataSetUtil.createDataSetColumn(dataSet, dataSet.getShowIndex(j)));
-			}else{
-			   //直接赋值
-				context.put(field.getName(), dataSet.getData(dataSet.getShowIndex(row), dataSet.getShowIndex(j)));  //设置行参数上下文
+			if (columns != null && columns.contains(j)) {
+				// 存在字段数组形式
+				context.put(field.getName(), DataSetUtil.createDataSetColumn(dataSet, dataSet.getShowIndex(j)));
+			} else {
+				// 直接赋值
+				context.put(field.getName(), dataSet.getData(dataSet.getShowIndex(row), dataSet.getShowIndex(j))); // 设置行参数上下文
 			}
-			
+
 		}
 	}
 
