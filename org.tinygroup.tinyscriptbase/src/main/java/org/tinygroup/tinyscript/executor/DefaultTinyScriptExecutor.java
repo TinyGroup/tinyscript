8c21c01d1d989d3bd7129a586940ddbbf723f04f
aotianji1238 <aotianji1238@163.com>
2017-10-30 14:33:27 +0800

    [重构] 脚本执行器重构，支持将spring加载相关日志重定向到脚本同级的spring.log，避免控制台输出大量无关信息

--- org.tinygroup.tinyscriptbase/src/main/java/org/tinygroup/tinyscript/executor/DefaultTinyScriptExecutor.java
+++ org.tinygroup.tinyscriptbase/src/main/java/org/tinygroup/tinyscript/executor/DefaultTinyScriptExecutor.java
@@ -1,5 +1,8 @@
 package org.tinygroup.tinyscript.executor;
 
+import java.io.File;
+import java.io.PrintStream;
+
 import org.tinygroup.tinyrunner.Runner;
 import org.tinygroup.tinyscript.ScriptException;
 
@@ -18,10 +21,52 @@ public class DefaultTinyScriptExecutor {
 					.println("Usage:\n\tDefaultTinyScriptExecutor tinyScriptFile [relativePath] [absolutePath] [resources] ");
 			return;
 		}
+		//获取重定向文件
+		File logfile = getLogFile(args);
+		
 		//初始化Spring上下文
-		Runner.init("/application.xml", null);
+		initSpring(logfile);
+				
 		//执行模拟器
 		operator.execute(args);
+	}
+	
+	
+	private static File getLogFile(String[] args) throws ScriptException {
+		String fileName = "spring.log";
+		if(args!=null && args.length>=2 && args[1]!=null){
+		   //定向文件和脚本文件同级目录
+		   String absolutePath = args[1].replaceAll("\\\\", "/");
+		   String newPath = absolutePath.substring(0, absolutePath.lastIndexOf("/"))+"/"+fileName;
+		   return new File(newPath);
+		}else{
+		   //没有配置路径
+		   return new File(fileName);
+		}
+	}
+	
+	private static void initSpring(File logfile) throws ScriptException {
+		PrintStream old = System.out;
+		PrintStream newlog = null;
+		if(logfile.exists()){
+		   logfile.delete();
+		}
+		try{
+			//重定向输出流到文件
+			newlog = new PrintStream(logfile);
+			System.setOut(newlog);
+			//加载配置逻辑
+			Runner.init("/application.xml", null);
+		} catch (Exception e) {
+			throw new ScriptException(e);
+		}finally{
+			//重定向输出流为原始流
+			System.setOut(old);
+			if(newlog!=null){
+			   newlog.close();	
+			}
+		}
+
 	}
 	
 }
